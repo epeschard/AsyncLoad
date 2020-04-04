96fe336ba48de609345944e316dae3e89b8884a2
Eugène Peschard <eugene.peschard@dowjones.com>
2020-04-04 15:11:59 +0200

    Add API structure

---
+++ Sources/AsyncLoad/API.swift
@@ -0,0 +1,63 @@
+//
+//  APICall.swift
+//  Networking
+//
+//  Created by Alexey Naumov on 23.10.2019.
+//  Copyright © 2019 Alexey Naumov. All rights reserved.
+//
+
+import Foundation
+
+public protocol API {
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    func body() throws -> Data?
+}
+
+public enum APIError: Swift.Error {
+    case invalidURL
+    case httpCode(HTTPCode)
+    case unexpectedResponse
+}
+
+extension APIError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .invalidURL: return "Invalid URL"
+        case let .httpCode(code): return "Unexpected HTTP code: \(code)"
+        case .unexpectedResponse: return "Unexpected response from the server"
+        }
+    }
+}
+
+public extension API {
+    func urlRequest(baseURL: String,
+                    with token: String? = nil)
+        throws -> URLRequest {
+        guard let url = URL(string: baseURL + path) else {
+            throw APIError.invalidURL
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.httpBody = try body()
+        request.setValue("application/json; charset=utf-8",
+                         forHTTPHeaderField: "Content-Type")
+        if let key = token {
+            request.setValue("token \(key)", forHTTPHeaderField: "Authorization")
+        }
+        return request
+    }
+}
+
+public typealias HTTPCode = Int
+public typealias HTTPCodes = Range<HTTPCode>
+
+public extension HTTPCodes {
+    static let success = 200 ..< 300
+    static let unauthorized = 401
+}
+
+public enum HTTPMethod: String {
+    case GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH, TRACE, CONNECT
+}
